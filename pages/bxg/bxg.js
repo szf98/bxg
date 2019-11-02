67ba1a8c55ef65f8f3a544ad66d6365812e3b4aa
1904336877@qq.com <1904336877@qq.com>
2019-11-02 23:53:56 +0800

    a

---
+++ pages/bxg/bxg.js
@@ -0,0 +1,77 @@
+// pages/bxg/bxg.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+      list1:[
+        {im:"https://m.boxuegu.com/web/images/index/course.png" , name:"就业课"},
+        {im:"https://m.boxuegu.com/web/images/index/mircoCourse.png" , name:"提升课"},
+        {im:"https://m.boxuegu.com/web/images/index/freeMicroCourse.png" , name:"免费课"},
+        {im:"https://m.boxuegu.com/web/images/index/courseType.png" , name:"课程介绍"}
+      ],
+      list2:[
+        {image:"https://file.boxuegu.com/f6c878722f394a7c9bd12c0319f00037.png",lanage:"javaEE",introduce:"java是一个开发性语言",peoples:"110人学习",money:"￥10000"},
+        {image:"https://file.boxuegu.com/e19c53c6c1fa468aad6e11cb61d81b9a.png",lanage:"python+人工智能",introduce:"最有前景的开发职位",peoples:"1230人学习",money:"￥9999"},
+        {image:"https://file.boxuegu.com/3940be1c755a44eeb8e2e0aaf984756e.png",lanage:"前端开发",introduce:"用户界面的构筑者",peoples:"887人学习",money:"￥10999"},
+        {image:"https://file.boxuegu.com/6c0ffe21a06d421d90be8a0061c14c1a.png",lanage:"软件测试",introduce:"",peoples:"556人学习",money:"￥9999"},
+      ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
